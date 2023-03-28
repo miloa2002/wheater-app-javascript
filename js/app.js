c4647cbb4279cc8ff972025060774e84bacf6dd7
miloa2002 <agudelo.tapias@gmail.com>
2023-03-27 23:44:12 -0500

    terminado proyecto con api

--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
 const formulario = document.querySelector(".formulario");
 const contenedor_busqueda = document.querySelector(".contenedor_busqueda");
+const btnSlide = document.querySelector(".btnSlide");
+const col_1 = document.querySelector(".col_1");
+const col_2 = document.querySelector(".col_2");
+const quitarSlide = document.querySelector(".quitarSlide");
+const titulo = document.querySelector(".titulo");
+const nombre_ciudad = document.querySelector(".nombre_ciudad");
+
+const nubosidad = document.querySelector(".nubosidad");
+const maxima = document.querySelector(".maxima");
+const minima = document.querySelector(".minima");
 
 const APIKEY = "c567b85da3d146a868ab3b33e667ff52";
+
+/*****ocultar y mostrar*** */
+btnSlide.addEventListener("click", () => {
+  if (col_1.style.display === "none") {
+    col_1.style.display = "block";
+    col_2.style.display = "none";
+  } else {
+    col_1.style.display = "none";
+  }
+});
+
+quitarSlide.addEventListener("click", () => {
+  if (col_2.style.display === "none") {
+    col_2.style.display = "block";
+    col_1.style.display = "none";
+  } else {
+    col_2.style.display = "none";
+  }
+});
+
+/*********consumo api***** */
 
 document.addEventListener("DOMContentLoaded", () => {
   formulario.addEventListener("submit", enviarFormulario);
@@ -41,5 +72,15 @@ async function obtenerDataClima(inputBusqueda) {
 }
 
 function mostrarData(data) {
-  console.log(data);
+  const {
+    main: { temp_max, temp_min, temp },
+    name,
+    clouds: { all },
+  } = data;
+
+  titulo.textContent = temp + "°K";
+  nombre_ciudad.textContent = name;
+  nubosidad.textContent = all;
+  minima.textContent = temp_min;
+  maxima.textContent = temp_max;
 }
